27d663097e21359925e2397389b56f2a7e87bc04
Shakhriyor <109933495+ShakhriyorIsobekov@users.noreply.github.com>
2024-06-26 00:16:22 +0500

    add next page

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const darkMode = document.querySelector(".header__content-btn");
+darkMode.addEventListener("click", function() {
+   document.body.classList.add('dark__mode');
+})
+
+// const lightMode = document.querySelector(".header__content-btnn");
+// lightMode.addEventListener("click", function() {
+//    document.body.classList.remove('dark__mode')
+// })
