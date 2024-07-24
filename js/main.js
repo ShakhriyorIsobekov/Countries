98b5c608ecc9837f102af8a996b845aad7213583
Shakhriyor <109933495+ShakhriyorIsobekov@users.noreply.github.com>
2024-07-24 16:49:40 +0500

    finish the work

--- js/main.js
+++ js/main.js
@@ -1,14 +1,4 @@
 const darkMode = document.querySelector(".header__content-btn");
 darkMode.addEventListener("click", function() {
-   document.body.classList.add('dark__mode');
-})
-
-// const lightMode = document.querySelector(".header__content-btnn");
-// lightMode.addEventListener("click", function() {
-//    document.body.classList.remove('dark__mode')
-// })
-
-const darkModeAbout = document.querySelector("header__content-btn");
-darkModeAbout.addEventListener("click", function() {
-   document.body.classList.add('dark__mode');
+   document.body.classList.toggle('dark__mode');
 })
