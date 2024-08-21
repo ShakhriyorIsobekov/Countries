1d5a24aba6f691b74c526054fbc4a9d6cd809535
Shakhriyor <109933495+ShakhriyorIsobekov@users.noreply.github.com>
2024-08-21 17:18:13 +0500

    make a website fully responsive

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 const darkMode = document.querySelector(".header__content-btn");
+
 darkMode.addEventListener("click", function() {
    document.body.classList.toggle('dark__mode');
 })
+
+
+// TODO mana shu pastdagi ko'dimni ko'p martda to'g'irlab ko'rdim lekin o'xshamayapti
+
+// const darkMode = document.querySelector(".header__content-btn");
+
+// darkMode.addEventListener("click", function() {
+//    localStorage.setItem('theme', 'dark__mode');
+// });
+
+// if (localStorage.getItem('theme') === 'dark__mode') {
+//    document.body.classList.add('dark__mode');
+// } else {
+//    document.body.classList.remove('dark__mode');
+// };
